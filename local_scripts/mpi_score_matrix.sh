926a186750d706ff9ccf1c096071e4e6247df332
Bo Feng <bfeng1216@gmail.com>
2018-08-24 09:57:20 -0400

    partial impl

--- local_scripts/mpi_score_matrix.sh
+++ local_scripts/mpi_score_matrix.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+cd ..
+mvn package
+
+cd local_scripts
+
+time mpirun -n 2 java -cp ../target/bacterial-proteins-jar-with-dependencies.jar \
+        edu.indiana.sice.spidal.apps.MPIScoreMatrixGenerator \
+        ../data/data.tsv \
+        ../output
--- src/main/java/edu/indiana/sice/spidal/apps/MPIScoreMatrixGenerator.java
+++ src/main/java/edu/indiana/sice/spidal/apps/MPIScoreMatrixGenerator.java
@@ -2,13 +2,17 @@ package edu.indiana.sice.spidal.apps;
 
 import mpi.MPI;
 import mpi.MPIException;
+import mpi.Status;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.IntBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class MPIScoreMatrixGenerator {
@@ -39,10 +43,14 @@ public class MPIScoreMatrixGenerator {
     private static int worldProcRank = 0;
     private static int MASTER = 0;
     private static String source;
+    private static String outputDir;
 
     private static long nLines = 0;
     private static long rowStart;
     private static long rowEnd;
+
+
+    private static Map<Integer, Integer> indexMap;
 
     private static void allPrintln(final String msg) {
         try {
@@ -62,6 +70,7 @@ public class MPIScoreMatrixGenerator {
         worldProcCount = MPI.COMM_WORLD.getSize();
         worldProcRank = MPI.COMM_WORLD.getRank();
         source = args[0];
+        outputDir = args[1];
     }
 
     private static void partitionByRank() {
@@ -87,6 +96,13 @@ public class MPIScoreMatrixGenerator {
         MPI.COMM_WORLD.barrier();
     }
 
+    private static int allReduceMax(int value) throws MPIException {
+        IntBuffer intBuffer = MPI.newIntBuffer(1);
+        intBuffer.put(0, value);
+        MPI.COMM_WORLD.allReduce(intBuffer, 1, MPI.INT, MPI.MAX);
+        return intBuffer.get(0);
+    }
+
     private static void buildIndices() throws MPIException, IOException {
         Set<Integer> integerSet = new HashSet<>();
         String line;
@@ -104,6 +120,55 @@ public class MPIScoreMatrixGenerator {
         }
         int[] indexArray = integerSet.stream().mapToInt(Integer::intValue).toArray();
         allPrintln("Indices.length=" + indexArray.length);
+
+        // Reduce index arrays to MASTER rank
+        int BUF_SIZE = allReduceMax(indexArray.length);
+        MPI.COMM_WORLD.barrier();
+        if (worldProcRank == MASTER) {
+            int[] bufArray = new int[BUF_SIZE];
+            for (int i = 0; i < worldProcCount; i++) {
+                if (i != MASTER) {
+                    Status status = MPI.COMM_WORLD.recv(bufArray, BUF_SIZE, MPI.INT, i, i);
+                    int realSize = status.getCount(MPI.INT);
+                    System.out.println(String.format("Rank[%d]:%d size received", MASTER, realSize));
+                    for (int j = 0; j < realSize; j++) {
+                        integerSet.add(bufArray[j]);
+                    }
+                }
+            }
+        } else {
+            MPI.COMM_WORLD.send(indexArray, indexArray.length, MPI.INT, MASTER, worldProcRank);
+            System.out.println(String.format("Rank[%d]:%d size sent", worldProcRank, indexArray.length));
+        }
+        MPI.COMM_WORLD.barrier();
+
+        // Broadcast indices to all slaves
+        if (worldProcRank == MASTER) {
+            indexArray = integerSet.stream().mapToInt(Integer::intValue).toArray();
+        }
+        MPI.COMM_WORLD.barrier();
+        BUF_SIZE = allReduceMax(indexArray.length);
+        if (worldProcRank != MASTER) {
+            indexArray = new int[BUF_SIZE];
+        }
+        MPI.COMM_WORLD.bcast(indexArray, indexArray.length, MPI.INT, MASTER);
+        allPrintln("Indices.length=" + indexArray.length);
+
+        // Build local indices
+        indexMap = new HashMap<>(indexArray.length);
+        for (int i = 0; i < indexArray.length; i++) {
+            indexMap.put(indexArray[i], i);
+        }
+
+        allPrintln("IndexMap.size=" + indexMap.size());
+    }
+
+    private static void dumpPartitionFiles(String input, String outputDir) throws IOException {
+        assert indexMap != null;
+
+        String matrixPath = String.format("%s/%d-matrix-file-part-%04d.bin", outputDir, indexMap.size(), worldProcRank);
+
+        allPrintln("Matrix partition:" + matrixPath);
     }
 
     public static void main(String[] args) throws MPIException, IOException {
@@ -114,6 +179,8 @@ public class MPIScoreMatrixGenerator {
         partitionByRank();
         allPrintln(String.format("Total # of lines:%d, rowStart=%d, rowEnd=%d", nLines, rowStart, rowEnd));
         buildIndices();
+
+        dumpPartitionFiles(source, outputDir);
 
         MPI.Finalize();
     }
