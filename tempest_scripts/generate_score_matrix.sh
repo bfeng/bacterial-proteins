24fef49cfe480476e5b366114250209952080b32
Bo Feng <bfeng1216@gmail.com>
2018-09-06 11:40:34 -0400

    seperate script

---
+++ tempest_scripts/generate_score_matrix.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+java -cp ../target/bacterial-proteins-jar-with-dependencies.jar \
+        edu.indiana.sice.spidal.apps.DataConverter \
+        ../data/fusion2.15k_sampled.tsv \
+        ../data/score-matrix.csv
