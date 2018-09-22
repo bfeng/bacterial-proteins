50cba77202aaa38e8db8304fc30d951328ca8809
Bo Feng <bfeng1216@gmail.com>
2018-09-21 21:35:00 -0400

    weight matrices gen

--- src/main/java/edu/indiana/sice/spidal/apps/DNADataConvert.java
+++ src/main/java/edu/indiana/sice/spidal/apps/DNADataConvert.java
@@ -8,30 +8,41 @@ public class DNADataConvert {
 
     private static String INPUT_MATRIX;
     private static String OUTPUT_MATRIX;
-
-    private static final double threshold = 0.8;
+    private static String WEIGHT_MATRIX;
+    private static double THRESHOLD = 0.99999;
 
     private static void run() throws IOException {
         InputStream inputStream = new FileInputStream(new File(INPUT_MATRIX));
         LittleEndianDataInputStream littleEndianDataInputStream = new LittleEndianDataInputStream(inputStream);
-        DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(new File(OUTPUT_MATRIX)));
+        DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(new File(OUTPUT_MATRIX))));
+        DataOutputStream weightOutputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(new File(WEIGHT_MATRIX))));
 
         while (littleEndianDataInputStream.available() > 0) {
             short value = littleEndianDataInputStream.readShort();
             double out = (double) value / Short.MAX_VALUE;
-            if (out <= threshold)
+            if (out <= THRESHOLD) {
                 dataOutputStream.writeShort(value);
-            else
-                dataOutputStream.writeShort(Short.MAX_VALUE);
+                weightOutputStream.writeShort((short) (0.998 * Short.MAX_VALUE));
+            } else {
+                dataOutputStream.writeShort(0);
+                weightOutputStream.writeShort(0);
+            }
         }
 
+        weightOutputStream.close();
         dataOutputStream.close();
         littleEndianDataInputStream.close();
         inputStream.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 4) return;
+
         INPUT_MATRIX = args[0];
         OUTPUT_MATRIX = args[1];
+        WEIGHT_MATRIX = args[2];
+        THRESHOLD = Double.parseDouble(args[3]);
+
+        run();
     }
 }
