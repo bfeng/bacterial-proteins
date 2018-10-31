69561c062c0db63bf51436d73d3bba620634424a
Bo Feng <bfeng1216@gmail.com>
2018-10-31 16:46:06 -0400

    added buf support

--- src/main/java/edu/indiana/sice/spidal/apps/CountZeroWeights.java
+++ src/main/java/edu/indiana/sice/spidal/apps/CountZeroWeights.java
@@ -2,12 +2,14 @@ package edu.indiana.sice.spidal.apps;
 
 import com.google.common.io.LittleEndianDataInputStream;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class CountZeroWeights {
 
+    private static final int BUF_SIZE = 819200;
     private long totalCounts;
     private long zeroCounts;
 
@@ -19,7 +21,14 @@ public class CountZeroWeights {
 
     private void run() {
         try {
-            LittleEndianDataInputStream dataInputStream = new LittleEndianDataInputStream(new FileInputStream(new File(weightMatrix)));
+            LittleEndianDataInputStream dataInputStream = new LittleEndianDataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(
+                                    new File(weightMatrix)
+                            ),
+                            BUF_SIZE
+                    )
+            );
 
             while (dataInputStream.available() > 0) {
                 short val = dataInputStream.readShort();
