5de9fc34ae58a7a022579ab81528dbc110141bd0
Bo Feng <bfeng1216@gmail.com>
2018-11-03 20:35:30 -0400

    more cases

--- local_scripts/append_all.sh
+++ local_scripts/append_all.sh
@@ -1,4 +1,4 @@
-for (( i = 1; i <= 16; i++ )); do
+for (( i = 1; i <= 22; i++ )); do
     case=$(printf "%02d" $i)
     bash ~/DSC-SPIDAL/bacterial-proteins/local_scripts/append_clusters.sh ${case}-damds-points.txt ~/DSC-SPIDAL/bacterial-proteins/results/DNA-data/from-pulasthi/region_2_with_dust.webplotviz > ${case}-damds-points.clusters.txt
 done
