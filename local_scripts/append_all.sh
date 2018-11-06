08890b30b3a665b8f133b9fc9db5cda854587702
Bo Feng <bfeng1216@gmail.com>
2018-11-06 12:54:53 -0500

    added shebang

--- local_scripts/append_all.sh
+++ local_scripts/append_all.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 for (( i = 1; i <= 22; i++ )); do
     case=$(printf "%02d" $i)
     bash ~/DSC-SPIDAL/bacterial-proteins/local_scripts/append_clusters.sh ${case}-damds-points.txt ~/DSC-SPIDAL/bacterial-proteins/results/DNA-data/from-pulasthi/region_2_with_dust.webplotviz > ${case}-damds-points.clusters.txt
